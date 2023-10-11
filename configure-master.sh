2927ef31dfe0d8963ea4c2329ddcc806d4249591
xperez2001 <xavierperezbdn@gmail.com>
2023-10-11 20:23:37 +0200

    1-5

--- configure-master.sh
+++ configure-master.sh
@@ -1,14 +1,40 @@
 #!/bin/bash
 
-echo "Introduce el nombre del host"
+mIP=$(hostname -I | awk '{print $2}')
+mIPI=$(hostname -I | awk '{print $1}')
+
+echo "Nombre del host"
 read mName
-
-#hostnamectl set-hostname $mName
-
-echo "Introduce la IP y el nombre del nodo worker"
+echo "IP y nombre del nodo worker"
 read wIP wName
+sleep 1
 
+
+echo "Actualizando el hostname..."
+hostnamectl set-hostname $mName
+sleep 1
+
+
+echo "Actualizando la tabla de hosts..."
 echo "127.0.0.1    localhost
 127.0.1.1    $mName
 
-$wIP    $wName" #> etc/hosts
+$wIP    $wName" > etc/hosts
+sleep1
+
+
+echo "Activando el IP forwarding..."
+sed -i '/net.ipv4.ip_forward=1/s/#//g' /etc/sysctl.conf
+sleep 1
+
+
+echo "Actualizando las tablas nat..."
+iptables -t nat -A POSTROUTING -s 20.20.20.0/23 -o ens3 -j SNAT --to $mIPI
+iptables -t nat -A POSTROUTING -o ens3 -j MASQUERADE
+iptables -A FORWARD -i ens3 -o ens4 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i ens4 -o ens3 -j ACCEPT
+sleep 1
+
+
+echo "Haciendo efectivo el IP forwarding..."
+echo 1 > /proc/sys/net/ipv4/ip_forward
--- configure-worker.sh
+++ configure-worker.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 
-echo "Introduce el nombre del host"
+wIP=$(hostname -I)
+
+echo "Nombre del host"
 read wName
-
-#hostnamectl set-hostname $wName
-
-echo "\nIntroduce la IP y el nombre de la interfaz de Internet del nodo master"
+echo "IP y nombre del nodo master (Internet)"
 read mNameI mIPI
-echo "\nIntroduce la IP y el nombre de la interfaz Middle del nodo master"
+echo "IP y nombre del nodo master (Middle)"
 read mName mIP
 
+
+echo "Actualizando el hostname..."
+hostnamectl set-hostname $wName
+sleep 1
+
+
+echo "Actualizando la tabla de hosts..."
 echo "127.0.0.1    localhost
 127.0.1.1    $wName
 
 $mNameI    $mIPI
-$mName    $mIP" #> etc/hosts
+$mName    $mIP" > etc/hosts
+sleep 1
+
+
+echo "Actualizando el Gateway..."
+echo "auto lo
+iface lo inet loopback
+
+auto ens3
+iface ens3 inet static
+  address $wIP
+  network 20.20.20.0
+  netmask 255.255.254.0
+  gateway $mIP
+
+source /etc/network/interfaces.d/*.cfg" > etc/network/interfaces
+
+
